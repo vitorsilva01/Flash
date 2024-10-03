4dbb9d9db790ab82de345bcc3d6ade86dee755b7
vitotr chaves <149083552+vitorsilva01@users.noreply.github.com>
2024-10-03 17:32:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao (
+    'Relógio',
+    'Quando o relógio digital foi lançado ?',
+    '1961 , Por incrível que pareça, o relógio digital não é uma invenção dos tempos modernos. Thomas Bromley, um engenheiro britânico, criou o primeiro relógio digital em 1961, no norte da Inglaterra.'
+)
+criaCartao (
+    'Relógio',
+   'Qual a marca mais cara de relógio ?',
+    'Patek Philippe.'
+)
+
+criaCartao (
+    'Relógio',
+   'Como inventaram a hora?',
+    'Por volta de 1500 a.C., os egípcios desenvolveram um relógio de sol em forma de “T”, colocado no solo e calibrado para dividir o intervalo entre o nascer e o pôr do sol em 12 partes, com base no número de ciclos lunar.'
+)
+criaCartao (
+    ' Relógio',
+   'Quem inventou o relógio?',
+    'O primeiro instrumento para marcar as horas foi o relógio de sol , inventado pelos babilônios e egípcios.'
+)
+
